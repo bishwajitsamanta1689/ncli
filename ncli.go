bdc8c5382053501bd93bcf5a8a0d2cb39a72a4ed
bishwajitsamanta1689 <bishwajitsamanta1689@gmail.com>
2020-04-22 09:20:13 +0530

    Refactored the Golang code.

--- ncli.go
+++ ncli.go
@@ -1,4 +1,5 @@
 package main
+
 import (
 	"fmt"
 	"github.com/urfave/cli"
@@ -6,19 +7,31 @@ import (
 	"net"
 	"os"
 )
-func main() {
-	app:= cli.NewApp()
-	app.Name = "Website Lookup CLI"
-	app.Usage = "Let's you Query IP's, CNAME"
-	myFlags:= []cli.Flag {
-		&cli.StringFlag{
-			Name: "host",
-			Value: "",
-		},
+
+//Variable Declaration
+var app = cli.NewApp()
+var myFlags = []cli.Flag{
+	&cli.StringFlag{
+		Name:  "host",
+		Value: "",
+	},
+}
+
+func handleError(err error) {
+	if err != nil {
+		log.Fatal(err)
 	}
-	app.Commands =[]*cli.Command {
+}
+func info() {
+	app.Name = "Network CLI"
+	app.Usage = "Let's you Query IP's, CNAME"
+	app.Version = "0.1.0"
+}
+
+func command() {
+	app.Commands = []*cli.Command{
 		{
-			Name: "ns",
+			Name:  "ns",
 			Usage: "Looks up the Name Server for Particular Host",
 			Flags: myFlags,
 			Action: func(context *cli.Context) error {
@@ -26,15 +39,19 @@ func main() {
 				if err != nil {
 					return err
 				}
-				for i:=0; i<len(ns);i++ {
+				for i := 0; i < len(ns); i++ {
 					fmt.Println(ns[i].Host)
 				}
 				return nil
 			},
 		},
 	}
+}
+func main() {
+	// Importing Functions Declared
+	info()
+	command()
+
 	err := app.Run(os.Args)
-	if err != nil {
-		log.Fatal(err)
-	}
+	handleError(err)
 }
